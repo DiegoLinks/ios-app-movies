2aba78870de3090e0fa6e4adf10023dda1335c10
Diego <diego.rof@hotmail.com>
2023-10-10 12:31:12 -0300

    Upload project

---
+++ ios-app-movies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-app-movies
+//
+//  Created by Diego De Oliveira on 10/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-app-movies/ios_app_moviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_app_moviesApp.swift
+//  ios-app-movies
+//
+//  Created by Diego De Oliveira on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_app_moviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
